ab51720f5273e28f24cb875f83f1c20d9aaef333
Ke Ma <ke.ma@wearesuburb.com>
2016-04-28 12:36:34 +0100

    init push

---
+++ Gojimo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gojimo
+//
+//  Created by Ke Ma on 28/04/2016.
+//  Copyright © 2016 Ke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
